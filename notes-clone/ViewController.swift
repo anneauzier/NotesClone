5dd88ce9ac48b3d06b3191696235759abefd6e3b
anneauzier <anne.auziier@outlook.com>
2024-06-19 08:42:37 -0400

    chore: add .gitignore file

---
+++ notes-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  notes-clone
+//
+//  Created by Anne Auzier on 19/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
